e78a39c51dd82215a0f9617ee676907411b95a97
sean <sean.g.crim@gmail.com>
2019-06-10 19:08:42 -0400

    began the intro of the course. set up the environment, learned some background on nodejs, and also began to start understanding nodejs modules

---
+++ nodejs_module_system_notes_app/app.js
@@ -0,0 +1,34 @@
+/*const fs = require('fs');
+
+fs.writeFileSync('random.txt', 'this file was created by node.js');
+
+/**
+ * challenge: append a message to notes.txt
+ * 
+ * 1. use appendFileSync to append to the file
+ * 2. run the script
+ * 3. check that what has been appended is in the file
+ * 
+ */
+
+/*fs.appendFileSync('random.txt', 'hey this is some appended text! hooray!');*/
+const utils = require('./utils.js');
+const getNotes = require('./notes.js');
+const validator = require('validator');
+
+//const name = 'sean';
+console.log(utils.name);
+console.log(utils.add(2, 3));
+
+/**
+ * challenge: define and use a function in a new file
+ * 
+ * 1. create new file called notes.js
+ * 2. create getNotes function that returns 'your notes...'
+ * 3. export getNotes function
+ * 4. from app.js, load in and call the function printing message to the console
+ */
+
+ console.log(getNotes());
+
+ console.log(validator.isEmail('theseancrim@gmail.com'));
